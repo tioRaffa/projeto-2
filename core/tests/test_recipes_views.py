cd6e6c4cb0cfcb8abf262336912e32b5700864fa
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-02-20 12:52:09 -0300

    base test

--- core/tests/test_recipes_views.py
+++ core/tests/test_recipes_views.py
@@ -1,38 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from core import views
-from core.models import Category, RecipesModels, User
 
-class RecipeTestBase(TestCase):
-    def setUp(self):
-        category = Category.objects.create(name='Teste')
-        
-        user = User.objects.create_user(
-            first_name='user',
-            last_name='user',
-            username='user nme',
-            password='user123',
-            email='user@gmail.com',
-            
-        )
-        
-        recipe = RecipesModels.objects.create(
-            category=category,
-            author=user,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='munitos',
-            servings=5,
-            servings_unit='Porcoes',
-            preparation_steps='recipe preparation steps',
-            preparation_steps_is_html=False,
-            is_published=True
-        )        
-        
-        return super().setUp()
-    
+from .recipe_test_base import RecipeTestBase
+
     
 class RecipeViewsTest(RecipeTestBase):
     
